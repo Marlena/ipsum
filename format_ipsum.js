6708312c06816cf4fef80ebbb4ada2fcd0fde3ca
Marlena Compton <marlena.compton@gmail.com>
2014-03-15 19:14:17 -0700

    added all tests for the checkbox

--- format_ipsum.js
+++ format_ipsum.js
@@ -7,19 +7,68 @@ function FormatIpsum(options) {
   //event delegate pattern
   $(this.el).on('submit', 'form', function(e) {
     e.preventDefault();
-    self.numParagraphs = $('#num_paragraphs').val();
+    self.numParagraphs = +$('#num_paragraphs').val();  //plus sign casts the string to a number
     self.render();
 
   });
 }
 
 FormatIpsum.prototype.render = function() {
-  var ipsum = new Ipsum(this.numParagraphs, ['apple', 'banna', 'pear', 'pineapple']);
+  var ipsum = new Ipsum(this.numParagraphs, words);
   var html = '';
-  $.each(ipsum, function() {
-    html += '<p>' + this + '</p>'
-  });
-  $('#for-paragraphs', this.el).html(html);
+  $('#errorText').text('');
 
-  return this;
+  if (this.numParagraphs > 0 && this.numParagraphs <= 100 && (this.numParagraphs === parseInt(this.numParagraphs,10))){
+    console.log(this.numParagraphs);
+    $.each(ipsum, function() {
+      html += '<p>' + this + '</p>'
+    });
+    $('#for-paragraphs', this.el).html(html);
+  }
+  else{
+
+   $('#errorText').text("Please submit a number between 1 and 100.");
+
+  }
+    return this;
+
 };
+
+//putting this at the bottom as it will be large and full of strings
+var words = ["Open your heart's eyes.",
+  "Take a deep breath.",
+  "Give yourself a break.",
+  "May you be healthy.",
+  "May you be safe.",
+  "Impermanence and change is a powerful teacher and teaching.",
+  "Who is really in pain when we hold on to resentments and grudges?",
+  "May you be at peace.",
+  "Live life one inhalation and one exhalation at a time.",
+  "Bring love into your heart, into your breath and into your being.",
+  "Reflect on the fragility and preciousness of life.",
+  "Stop and focus on whatever is being carried within you and let be.",
+  "Briefly notice any emotions, thoughts or sensations that may be driving fear and anxiety and let them be.",
+  "This discomfort will pass.",
+  "Slow down and ask yourself what is really happening.",
+  "Feelings of panic are uncomfortable, but they will not harm you.",
+  "Open your heart's eyes.",
+  "Stop and take a slow breath.",
+  "Inhale slowly and exhale slowly.",
+  "Exhale and let the muscles in your shoulders drop and relax.",
+  "Anxiety and panic will pass.",
+  "Panic is your body revving up temporarily, but it will slow down.",
+  "Let the muscles in your neck and shoulders relax.",
+  "You can do this.",
+  "You can get through this.",
+  "You can do what you set out to do; yes, you can.",
+  "Watch each breath appear and disappear, just breathing.",
+  "Hear the internal and external sounds around you rise and fall.",
+  "Open your heart to love as boundless as the sun, the moon, the stars.",
+  "Open your heart to change, forgiveness and lovingkindness.",
+  "Just acknowledge what's there and let be.",
+  "Let go of the need to analyze and let be.",
+  "Love is the first seed of the soul.",
+  "Empty your mind; be formless, shapeless like water.",
+  "Picture yourself releasing the burden you feel from sorrow, regret or resentment."];
+
+
--- spec/format_ipsum_spec.js
+++ spec/format_ipsum_spec.js
@@ -1,7 +1,7 @@
 describe("Format Ipsum takes an array of paragraphs and formats them for html", function() {
   var subject;
   beforeEach(function() {
-    jasmine.content().append('<form name="submit_ipsum" method="POST" class="blah"> <div> How many paragraphs? <input id="num_paragraphs" name="num_paragraphs" type="text" size="5" value="1"  /> <input type="submit" value="Breathe"/> <div id="for-paragraphs"></div> </div> </form>');
+    jasmine.content().append('<form name="submit_ipsum" method="POST" class="blah"> <div> How many paragraphs? <input id="num_paragraphs" name="num_paragraphs" type="text" size="5" value="1"  /> <input type="submit" value="Breathe"/> <span id="errorText"></span> <div id="for-paragraphs"></div> </div> </form>');
 
     subject = new FormatIpsum({el: jasmine.content()[0]});
     subject.render();
@@ -20,5 +20,52 @@ describe("Format Ipsum takes an array of paragraphs and formats them for html", 
     it('should change the paragraph count', function() {
       expect($('#for-paragraphs p').length).toEqual(3);
     });
+
+    describe('when an invalid value is submitted,', function(){
+
+
+      it ('should return an error message if the value is less than 1', function(){
+        submitForm(0);
+        expect($('#errorText').text()).toBe("Please submit a number between 1 and 100.");
+      });
+    });
+
+      it ('should return an error message if the value is greater than 100', function(){
+        submitForm(101);
+        expect($('#errorText').text()).toBe("Please submit a number between 1 and 100.");
+
+      });
+
+      it ('should return an error message if the value is not whole digits', function(){
+        submitForm(3.14);
+        expect($('#errorText').text()).toBe("Please submit a number between 1 and 100.");
+
+      });
+
+      it ('should return an error message if the value is not a number' , function(){
+        submitForm("blah");
+        expect($('#errorText').text()).toBe("Please submit a number between 1 and 100.");
+
+      });
+
+    it ('should return an error message if the value is not a number' , function(){
+      submitForm("abc");
+      expect($('#errorText').text()).toBe("Please submit a number between 1 and 100.");
+
+    });
+
+
+      var submitForm = function (numParagraphs){
+        $('#num_paragraphs').val(numParagraphs);
+        $('form').submit();
+      };
+
+
   });
+
+
+
+
+
+
 });
